12050033c006b34aaf49bca796877e7366a99a23
Huy Bui <huy.bui@multicorewareinc.com>
2012-07-16 17:31:26 -0500

    [Bug #4] cannot regconize TOKEN_CREGISTER

---
+++ build/tokens.h
@@ -0,0 +1,1 @@
+enum token_type { TOKEN_CREGISTER= 258 };  // begin from 258 to avoid conflict with ASCII characters
--- test/hsail_test.cc
+++ test/hsail_test.cc
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "lexer.h"
 #include <string>
-
+#include "tokens.h"
 
 
 TEST(LexTest, Bug1) {
@@ -14,4 +14,11 @@ TEST(LexTest, Bug2) {
   std::string input("12345");
   yy_scan_string((char*)input.c_str());
   EXPECT_EQ(2,yylex());
+}
+
+
+TEST(LexTest, Bug4) {
+  std::string input("$c7");
+  yy_scan_string((char*)input.c_str());
+  EXPECT_EQ(TOKEN_CREGISTER,yylex());
 }
