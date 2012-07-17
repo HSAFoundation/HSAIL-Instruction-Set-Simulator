e9bc88ab000ed291bedd1913024696600a90d385
Huy Bui <huy.bui@multicorewareinc.com>
2012-07-17 10:26:00 -0500

    [Bug #6] lexer cannot recognize TOKEN_SREGISTER

--- test/hsail_test.cc
+++ test/hsail_test.cc
@@ -28,3 +28,9 @@ TEST(LexTest, Bug5) {
   yy_scan_string((char*)input.c_str());
   EXPECT_EQ(TOKEN_DREGISTER,yylex());
 }
+
+TEST(LexTest, Bug6) {
+  std::string input("$s15");
+  yy_scan_string((char*)input.c_str());
+  EXPECT_EQ(TOKEN_SREGISTER,yylex());
+}
