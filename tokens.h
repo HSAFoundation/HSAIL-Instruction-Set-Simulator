4c19b49e72e24cf6ec6f9402c37817b578d8d877
Miao Wang <none@none>
2012-07-17 11:39:04 -0500

    [Fix #14] Add TOKEN_INTEGER_CONSTANT

--- tokens.h
+++ tokens.h
@@ -1,4 +1,5 @@
 enum token_type { TOKEN_CREGISTER= 258 ,
-				  TOKEN_DREGISTER,
-				  TOKEN_SREGISTER
-				 };  // begin from 258 to avoid conflict with ASCII characters
+		  TOKEN_DREGISTER,
+		  TOKEN_SREGISTER,
+		  TOKEN_INTEGER_CONSTANT
+		 };  // begin from 258 to avoid conflict with ASCII characters
