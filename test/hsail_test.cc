9963d7e2faef12b7e04102b9ae4aacefc8f9b2a2
Miao Wang <none@none>
2012-07-17 11:36:56 -0500

    [Bug #14] Do not have a TOKEN_CONSTANT_INT,

--- test/hsail_test.cc
+++ test/hsail_test.cc
@@ -3,6 +3,7 @@
 #include <string>
 #include "../tokens.h"
 
+extern int int_val;
 
 TEST(LexTest, Bug1) {
   std::string input("foo");
@@ -13,13 +14,13 @@ TEST(LexTest, Bug1) {
 TEST(LexTest, Bug2) {
   std::string input("12345");
   yy_scan_string((char*)input.c_str());
-  EXPECT_EQ(2,yylex());
+  EXPECT_EQ(TOKEN_INTEGER_CONSTANT,yylex());
 }
 
 TEST(LexTest, Bug3) {
   std::string input("020");
   yy_scan_string((char*)input.c_str());
-  EXPECT_EQ(3,yylex());
+  EXPECT_EQ(TOKEN_INTEGER_CONSTANT,yylex());
 }
 TEST(LexTest, Bug4) {
   std::string input("$c7");
@@ -42,7 +43,7 @@ TEST(LexTest, Bug6) {
 TEST(LexTest, Bug11) {
   std::string input("0x2f");
   yy_scan_string((char*)input.c_str());
-  EXPECT_EQ(4,yylex());
+  EXPECT_EQ(TOKEN_INTEGER_CONSTANT,yylex());
 }
 TEST(LexTest, Bug12) {
   std::string input("0.5e3f");
