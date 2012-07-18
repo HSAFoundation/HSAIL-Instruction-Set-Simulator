16a332d3a03776d71e1fe8db073b0bd45f0625c7
Huy Bui <huy.bui@multicorewareinc.com>
2012-07-18 13:35:27 -0500

    [Clean up]

--- parser.cc
+++ parser.cc
@@ -3,28 +3,28 @@
 #include "build/lexer.h"
 #include <string>
 
-int parse(std::string input) {
-  yy_scan_string((char*) input.c_str());
-  int token = yylex();   // get first token
-  int result;
+int parse(std::string input)
+{
+    yy_scan_string((char*) input.c_str());
+    int token = yylex();   // get first token
+    int result;
 
-  terminal_type term = get_token_type(token);
-  switch (term) {
+    terminal_type term = get_token_type(token);
+    switch (term) {
     case QUERY_OP:
-      // have a query
-      // check syntax
-      result = query(token);    
-      return result;
+        // have a query
+        // check syntax
+        result = query(token);
+        return result;
     default:
-      return 1;
-  }
+        return 1;
+    }
 }
 
 terminal_type get_token_type(int token)
 {
-  switch (token)
-  {
-    /* DataTypeId */
+    switch (token) {
+        /* DataTypeId */
     case _U32:
     case _S32:
     case _S64:
@@ -64,10 +64,9 @@ terminal_type get_token_type(int token)
     case _F64X2:
     case _S64X2:
     case _U64X2:
-      printf("dataTypeId\n");
-      return DATA_TYPE_ID;
+        return DATA_TYPE_ID;
 
-    /* queryOp */
+        /* queryOp */
     case QUERY_ORDER:
     case QUERY_DATA:
     case QUERY_ARRAY:
@@ -75,218 +74,144 @@ terminal_type get_token_type(int token)
     case QUERY_DEPTH:
     case QUERY_NORMALIZED:
     case QUERY_FILTERING:
-      printf("queryOp\n");
-      return QUERY_OP;
-   
-    /* Register */
+        return QUERY_OP;
+
+        /* Register */
     case TOKEN_CREGISTER:
     case TOKEN_DREGISTER:
     case TOKEN_SREGISTER:
     case TOKEN_QREGISTER:
-	  printf("Register\n");
-	  return REGISTER;
+        return REGISTER;
 
-
- 
-     default:
-	return UNKNOWN_TERM;  // unknown
-  }
+		default:
+        return UNKNOWN_TERM;  // unknown
+    }
 }
 
 int query(int queryOp)
 {
-  // next token should be a dataTypeId  
-  if (get_token_type(yylex()) != DATA_TYPE_ID)
-    return 1;
-  
-  // next token should be an operand
-  if (operand(yylex())) {
-     printf("invalid first operand\n");
-     return 1; 		  // invalid operand
-  }
-  // next should be a comma
-  if (yylex() != ',') {
-    printf("Missing comma\n"); 
-    return 1;
-  }
-  // then finally an addressable operand
-  if (addressableOperand(yylex())) {
-    printf("second operand is invalid\n");
-    return 1;
-  }
-
-  return 0;
-  
-}
-
-
-int operand(int first_token) {
-  if (!identifier(first_token))   // an identifier
-    return 0;      
-  else if (!baseOperand(first_token)) // a base operand
-    return 0;
-
-
-  return 1;
-
-}
-
-int identifier(int first_token) {
-  if (first_token == TOKEN_GLOBAL_IDENTIFIER)
-    return 0;
-  else if (first_token == TOKEN_LOCAL_IDENTIFIER)
-    return 0;
-  else if (get_token_type(first_token)==REGISTER)
-    return 0;
-
-
-
-  return 1;
-}
-
-int baseOperand(int first_token) {
-  if (first_token == TOKEN_DOUBLE_CONSTANT) 
-  {
-    printf("double constant\n");
-    return 0;
-  }
-  else if (first_token == TOKEN_SINGLE_CONSTANT)
-  {
-	printf("single constant\n");
-    return 0;
-  }
-  else if (first_token == TOKEN_INTEGER_CONSTANT)
-  {
-	printf("integer constant\n");
-    return 0;
-  }
-  else if (first_token == TOKEN_WAVESIZE)
-  {
- 	printf("TOKEN_WAVESIZE\n");
-    return 0;
-  } 
-  else if (first_token == '-')
-  {
-     if (yylex() == TOKEN_INTEGER_CONSTANT)
-		printf("integer constant\n");
-       return 0;
-  }
-  else if (get_token_type(first_token) == DATA_TYPE_ID )
-  {
-	 // scan next token
-     if (yylex() != '(') { // should be '('
-        printf("Missing '(' \n");   
- 		return 1;
-     }
-     else {
-         // check if we have a decimal list single or float list single
-     }
-
-  }
- 
-}
-
-int addressableOperand(int first_token) {
-  if (first_token != '[') {
-    printf ("Missing '['\n");
-    return 1;
-  }
-
-  if ((yylex() != TOKEN_GLOBAL_IDENTIFIER)&& (yylex() != TOKEN_LOCAL_IDENTIFIER)) {
-     printf ("Missing nonRegister\n");
-     return 1;
-  }
-  int c = yylex();
-  if (c == ']')
-  {
-     printf ("addressableOperand\n");
-     return 0;
-  }
-  else if (c == '<')
-  {
-     c  = yylex();
-     if (c == TOKEN_INTEGER_CONSTANT) 
-     { 
-
-        if (yylex() != '>')
-	{ 
-	   printf("Missing '>'\n");
-           return 1;
+    // next token should be a dataTypeId
+    if (get_token_type(yylex()) == DATA_TYPE_ID)
+	{
+		// next token should be an operand
+		if (!operand(yylex())) 
+		{
+			// next should be a comma
+			if (yylex() == ',') 
+			{
+				// then finally an addressable operand
+				if (!addressableOperand(yylex())) 
+				{
+					return 0;
+				}
+			}
+		}
 	}
-        if (yylex() != ']')
-	{ 
-	   printf("Missing ']'\n");
-           return 1;
-	}
-        printf ("addressableOperand\n");
+	
+    return 1;
+
+}
+
+
+int operand(int first_token)
+{
+    if (!identifier(first_token))   // an identifier
         return 0;
-     }
-     else if (get_token_type(c) == REGISTER)
-     {
-         c= yylex();
-         if (c=='>') {
-           if (yylex() == ']')
-           {
-              printf("addressableOperand\n");
-              return 0;
-   	       } 
-           else return 1;
-         }
-         else {
-           if  (c == '+') {
-             if (yylex()==TOKEN_INTEGER_CONSTANT) {
-				if (yylex()=='>') {
-           			if (yylex() == ']')
-           			{
-              			printf("addressableOperand\n");
-		                return 0;
-   	      		    } 
-           			else return 1;
-         		}
-
-             }
-             else 
-               return 1;
-          
-           }
-
-	else if  (c== '-') {
-             if (yylex()==TOKEN_INTEGER_CONSTANT) {
-				if (yylex()=='>') {
-           			if (yylex() == ']')
-           			{
-              			printf("addressableOperand\n");
-		                return 0;
-   	      		    } 
-           			else return 1;
-         		}
+    else if (!baseOperand(first_token)) // a base operand
+        return 0;
 
 
-             }
-             else 
-               return 1;
-          
-           }
-           
+    return 1;
+
+}
+
+int identifier(int first_token)
+{
+    if (first_token == TOKEN_GLOBAL_IDENTIFIER)
+        return 0;
+    else if (first_token == TOKEN_LOCAL_IDENTIFIER)
+        return 0;
+    else if (get_token_type(first_token)==REGISTER)
+        return 0;
 
 
 
+    return 1;
+}
 
-
+int baseOperand(int first_token)
+{
+    if (first_token == TOKEN_DOUBLE_CONSTANT) {
+        return 0;
+    } else if (first_token == TOKEN_SINGLE_CONSTANT) {
+        return 0;
+    } else if (first_token == TOKEN_INTEGER_CONSTANT) {
+        return 0;
+    } else if (first_token == TOKEN_WAVESIZE) {
+        return 0;
+    } else if (first_token == '-') {
+        if (yylex() == TOKEN_INTEGER_CONSTANT)
+            return 0;
+    } else if (get_token_type(first_token) == DATA_TYPE_ID ) {
+        // scan next token
+        if (yylex() == '(') { // should be '('
+            // check if we have a decimal list single or float list single
         }
+    }
+	else
+	{
+		return 1;
+	}
 
+}
 
-
-     }
-     else
-        return 1;
-    
-     
-      
-
-  }
-
-  else
-     return 1;
-
+int addressableOperand(int first_token)
+{
+	int next;
+    if (first_token == '[') 
+	{
+		// next should be a non register
+		if ( (yylex() == TOKEN_GLOBAL_IDENTIFIER)||(yylex() == TOKEN_LOCAL_IDENTIFIER) )
+		{
+			next = yylex();
+			
+			if (next == ']')
+				return 0;
+			else if (next == '<') 
+			{
+				next = yylex();
+				if (next == TOKEN_INTEGER_CONSTANT) 
+				{
+					if (yylex() == '>') 
+					{
+						if (yylex() == ']')
+							return 0;
+					}
+				
+				}
+				else if (get_token_type(next) == REGISTER) 
+				{
+					next = yylex();
+					if (next == '>')
+					{
+						if (yylex()==']')
+							return 0;
+					
+					}
+					else if ((next == '+') || (next == '-'))
+					{
+						if (yylex() == TOKEN_INTEGER_CONSTANT) 
+						{
+							if (yylex()=='>') 
+							{
+								if (yylex() == ']') 
+								    return 0;
+                            }
+						}
+					}
+				}
+			}
+		}
+    }
+	return 1;
 }
