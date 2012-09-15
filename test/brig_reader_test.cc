42ed76b46b38df27a2441a9ea488f0f46861abb4
Thomas B. Jablin <tom@multicorewareinc.com>
2012-09-14 22:50:23 -0500

    [HSA-368 Bug] Do not validate VectorCopy.bin

--- test/brig_reader_test.cc
+++ test/brig_reader_test.cc
@@ -1,4 +1,5 @@
 #include "brig_reader.h"
+#include "brig_module.h"
 
 #include "gtest/gtest.h"
 
@@ -9,6 +10,9 @@ TEST(BrigReaderTest, VectorCopy) {
     BrigReader::createBrigReader(TEST_PATH "/VectorCopy.bin");
   EXPECT_TRUE(reader);
   if(!reader) return;
+
+  hsa::brig::BrigModule mod(*reader, &llvm::errs());
+  EXPECT_TRUE(!mod.isValid());
 
   delete reader;
 }
