ca5588b592fb0f83f57b5f402d3e39bcf2d0a5d8
Thomas B. Jablin <tom@multicorewareinc.com>
2012-09-26 21:54:16 -0500

    [HSA-405 Fix] The fib function is now working correctly

--- test/brig_reader_test.cc
+++ test/brig_reader_test.cc
@@ -304,7 +304,10 @@ TEST(BrigWriterTest, Fib) {
     "  ld_private_u32 $s3, [%p]; // add in the saved result\n"
     "  add_u32 $s2, $s2, $s3;\n"
     "  st_arg_s32 $s2, [%r];\n"
-    "@return: ret;\n"
+    "  ret;\n"
+    "@return:"
+    "  st_arg_s32 1, [%r];\n"
+    "  ret;\n"
     "};\n"
     "\n"
     "kernel &fibKernel(kernarg_s32 %r_ptr, kernarg_s32 %n_ptr)\n"
@@ -312,11 +315,11 @@ TEST(BrigWriterTest, Fib) {
     "  {\n"
     "    arg_s32 %r;\n"
     "    arg_s32 %n;\n"
-    "    ld_kernarg_s32 $s0, [%r_ptr];\n"
-    "    st_arg_s32 $s1, [%n];\n"
+    "    ld_kernarg_s32 $s0, [%n_ptr];\n"
+    "    st_arg_s32 $s0, [%n];\n"
     "    call &fib(%r)(%n);\n"
-    "    ld_arg_s32 $s0, [%n];\n"
-    "    ld_kernarg_s32 $s1, [%n_ptr];\n"
+    "    ld_arg_s32 $s0, [%r];\n"
+    "    ld_kernarg_s32 $s1, [%r_ptr];\n"
     "	   st_global_s32 $s0, [$s1];\n"
     "  }\n"
     "  ret;\n"
@@ -324,6 +327,25 @@ TEST(BrigWriterTest, Fib) {
     );
   EXPECT_TRUE(mod);
   if(!mod) return;
+
+  int *r = new int;
+  int *n = new int;
+  void *args[] = { &r, n };
+  llvm::Function *fun = mod->getFunction("fibKernel");
+
+
+  int fib1 = 1;
+  int fib2 = 0;
+  for(int i = 1; i < 25; ++i) {
+    *n = i;
+    hsa::brig::launchBrig(mod, fun, args);
+    EXPECT_EQ(fib1 + fib2, *r);
+    fib1 = fib2;
+    fib2 = *r;
+  }
+
+  delete n;
+  delete r;
 }
 
 TEST(BrigWriterTest, VectorAbs) {
