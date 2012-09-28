8f859f41daa0da6a4b3c626facca3596bcd371a6
Thomas B. Jablin <tom@multicorewareinc.com>
2012-09-28 18:43:18 -0500

    [HSA-415] Initial Debug API implementation

---
+++ demo/debug.cc
@@ -0,0 +1,30 @@
+#include "AMDGPUDebug.h"
+
+#include <cstdio>
+
+static const char *getAPITypeStr(HwDbgAPIType apiType) {
+  if(apiType == HWDBG_API_HSA) {
+    return "HSA";
+  } else if(apiType == HWDBG_API_DIRECTX) {
+    return "DirectX";
+  } else {
+    return "Unknown";
+  }
+}
+
+int main(int argc, char **argv) {
+
+  unsigned major;
+  unsigned minor;
+  unsigned build;
+  HwDbgStatus status = HwDbgGetAPIVersion(&major, &minor, &build);
+  if(status != HWDBG_STATUS_SUCCESS) exit(-1);
+  printf("Debug API Verison %u.%u.%u\n", major, minor, build);
+
+  HwDbgAPIType apiType;
+  status = HwDbgGetAPIType(&apiType);
+  if(status != HWDBG_STATUS_SUCCESS) exit(-1);
+  printf("Debug API Type: %s\n", getAPITypeStr(apiType));
+
+  return 0;
+}
