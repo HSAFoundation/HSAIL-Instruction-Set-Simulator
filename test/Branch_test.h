74f0172f56f1869993788d45da2633ac1d7efb22
Chuang <none@none>
2012-10-26 15:54:31 +0800

    [HSA-438 Bug] Add more cases for Brn&Cbr rule, but can't generate  right brig code in Cbr rule.

--- test/Branch_test.h
+++ test/Branch_test.h
@@ -49,13 +49,13 @@ public:
     if (RefSrc2 != NULL) {
       const T2 *getsrc2 = reinterpret_cast <const T2*> 
                          (&(TestOutput->operands[getinst->o_operands[opCount++]]));
-      validate_brig::validate(RefSrc2, refbuf, getsrc2, getbuf);
+      validate_brig::validateOpType<T2>(RefSrc2, refbuf, getsrc2, getbuf);
     }
 
     if (RefSrc3 != NULL) {
       const T3 *getsrc3 = reinterpret_cast <const T3*> 
                          (&(TestOutput->operands[getinst->o_operands[opCount++]]));
-      validate_brig::validate(RefSrc3, refbuf, getsrc3, getbuf);
+      validate_brig::validateOpType(RefSrc3, refbuf, getsrc3, getbuf);
     }
     
     while (opCount <= 4) {
@@ -68,15 +68,17 @@ public:
 TEST(CodegenTest, Brn_Codegen){
 
   /********************* Common variables **********************/
-  std::string in, op1, op2; 
+  std::string in, regName; 
   StringBuffer* sbuf = new StringBuffer();
   BrigOperandImmed width;
   BrigOperandLabelRef labRef;
   BrigInstBase outInst;
   BrigInstMod outMod;
+  BrigOperandReg reg;
+  BrigOperandAddress addr;
 
-  /********************* Case 1 ********************************/
-  in.assign( "brn @loop;\n");
+  /*************************** Case 1 ********************************/
+  in.assign("brn @loop;\n");
 
   width.size = sizeof(width);
   width.kind = BrigEOperandImmed;
@@ -105,8 +107,8 @@ TEST(CodegenTest, Brn_Codegen){
   TestCase1.Run_Test(&Branch);  
   sbuf->clear();
 
-  /********************* Case 2 ********************************/
-  in.assign( "brn_width(all)_fbar @label;\n");
+  /*************************** Case 2 ********************************/
+  in.assign("brn_width(all)_fbar @label;\n");
   outMod.size = sizeof(outMod);
   outMod.kind = BrigEInstMod;
   outMod.opcode = BrigBrn;
@@ -118,6 +120,7 @@ TEST(CodegenTest, Brn_Codegen){
   outMod.o_operands[3] = 0;
   outMod.o_operands[4] = 0;
   memset(&outMod.aluModifier, 0, sizeof(outMod.aluModifier));
+  outMod.aluModifier.valid = 1;
   outMod.aluModifier.fbar = 1;
 
   width.size = sizeof(width);
@@ -134,8 +137,342 @@ TEST(CodegenTest, Brn_Codegen){
   Branch_Test<BrigInstMod, BrigOperandLabelRef> TestCase2(in, sbuf, &outMod, &width, &labRef);
   TestCase2.Run_Test(&Branch);  
   sbuf->clear();
+  /*************************** Case 3 ********************************/
+  in.assign("brn_width(0)_fbar $s1;\n");
+  regName.assign("$s1");  sbuf->append(regName);
+  
+  outMod.size = sizeof(outMod);
+  outMod.kind = BrigEInstMod;
+  outMod.opcode = BrigBrn;
+  outMod.type = Brigb32;
+  outMod.packing = BrigNoPacking;
+  outMod.o_operands[0] = 0;
+  outMod.o_operands[1] = sizeof(width);
+  outMod.o_operands[2] = 0;
+  outMod.o_operands[3] = 0;
+  outMod.o_operands[4] = 0;
+  memset(&outMod.aluModifier, 0, sizeof(outMod.aluModifier));
+  outMod.aluModifier.valid = 1;
+  outMod.aluModifier.fbar = 1;
 
-  /******************************  End of tests *****************************************/
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 0;
+  
+  reg.size = sizeof(reg);
+  reg.kind = BrigEOperandReg;
+  reg.type = Brigb32;
+  reg.reserved = 0;
+  reg.s_name = 0;
+
+  Branch_Test<BrigInstMod, BrigOperandReg> TestCase3(in, sbuf, &outMod, &width, &reg);
+  TestCase3.Run_Test(&Branch);  
+  sbuf->clear();
+
+  /*************************** Case 4 ********************************/
+  in.assign("brn_width(64) $s0;\n");
+  regName.assign("$s0");  sbuf->append(regName);
+  
+  outInst.size = sizeof(outInst);
+  outInst.kind = BrigEInstBase;
+  outInst.opcode = BrigBrn;
+  outInst.type = Brigb32;
+  outInst.packing = BrigNoPacking;
+  outInst.o_operands[0] = 0;
+  outInst.o_operands[1] = sizeof(width);
+  outInst.o_operands[2] = 0;
+  outInst.o_operands[3] = 0;
+  outInst.o_operands[4] = 0;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 64;
+  
+  reg.size = sizeof(reg);
+  reg.kind = BrigEOperandReg;
+  reg.type = Brigb32;
+  reg.reserved = 0;
+  reg.s_name = 0;
+
+  Branch_Test<BrigInstBase, BrigOperandReg> TestCase4(in, sbuf, &outInst, &width, &reg);
+  TestCase4.Run_Test(&Branch);
+  sbuf->clear();
+
+  /*************************** Case 5 ********************************/
+  in.assign("brn $s1, [%jumptable2];\n");
+  regName.assign("$s1");  sbuf->append(regName);
+  
+  outInst.size = sizeof(outInst);
+  outInst.kind = BrigEInstBase;
+  outInst.opcode = BrigBrn;
+  outInst.type = Brigb32;
+  outInst.packing = BrigNoPacking;
+  outInst.o_operands[0] = 0;
+  outInst.o_operands[1] = sizeof(width);
+  outInst.o_operands[2] = sizeof(width) + sizeof(reg);
+  outInst.o_operands[3] = 0;
+  outInst.o_operands[4] = 0;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 0;
+  
+  reg.size = sizeof(reg);
+  reg.kind = BrigEOperandReg;
+  reg.type = Brigb32;
+  reg.reserved = 0;
+  reg.s_name = 0;
+
+  addr.size = sizeof(addr);
+  addr.kind = BrigEOperandAddress;
+  addr.type = Brigb64;
+  addr.reserved = 0;
+  addr.directive = 0;
+
+  Branch_Test<BrigInstBase, BrigOperandReg, BrigOperandAddress> TestCase5(in, sbuf, &outInst, &width, &reg, &addr);
+  TestCase5.Run_Test(&Branch);
+  sbuf->clear();
+
+  /*************************** Case 6 ********************************/
+  in.assign("brn_fbar $s3, [@targets];\n");
+  regName.assign("$s3");  sbuf->append(regName);
+  
+  outMod.size = sizeof(outMod);
+  outMod.kind = BrigEInstMod;
+  outMod.opcode = BrigBrn;
+  outMod.type = Brigb32;
+  outMod.packing = BrigNoPacking;
+  outMod.o_operands[0] = 0;
+  outMod.o_operands[1] = sizeof(width);
+  outMod.o_operands[2] = sizeof(width) + sizeof(reg);
+  outMod.o_operands[3] = 0;
+  outMod.o_operands[4] = 0;
+  memset(&outMod.aluModifier, 0, sizeof(outMod.aluModifier));
+  outMod.aluModifier.valid = 1;
+  outMod.aluModifier.fbar = 1;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 0;
+  
+  reg.size = sizeof(reg);
+  reg.kind = BrigEOperandReg;
+  reg.type = Brigb32;
+  reg.reserved = 0;
+  reg.s_name = 0;
+
+  labRef.size = sizeof(labRef);
+  labRef.kind = BrigEOperandLabelRef;
+  labRef.labeldirective = 0;
+
+  Branch_Test<BrigInstMod, BrigOperandReg, BrigOperandLabelRef> TestCase6(in, sbuf, &outMod, &width, &reg, &labRef);
+  TestCase6.Run_Test(&Branch);
+  sbuf->clear();
+
+  /***************************  End of tests ***********************/
+  delete sbuf;
+}
+
+
+TEST(CodegenTest, Cbr_Codegen){
+
+  /********************* Common variables **********************/
+  std::string in, reg1Name, reg2Name;
+  StringBuffer* sbuf = new StringBuffer();
+  BrigOperandImmed width;
+  BrigOperandLabelRef labRef;
+  BrigInstBase outInst;
+  BrigInstMod outMod;
+  BrigOperandReg reg1, reg2;
+  BrigOperandAddress addr;
+
+  /*************************** Case 1 ********************************/
+  in.assign("cbr_width(4) $c1, $s1, [@tab];\n");
+  reg1Name.assign("$c1");   reg2Name.assign("$s1");
+  sbuf->append(reg1Name);   sbuf->append(reg2Name);
+
+  outInst.size = sizeof(outInst);
+  outInst.kind = BrigEInstBase;
+  outInst.opcode = BrigCbr;
+  outInst.type = Brigb32;
+  outInst.packing = BrigNoPacking;
+  outInst.o_operands[0] = 0;
+  outInst.o_operands[1] = sizeof(width);
+  outInst.o_operands[2] = sizeof(width) + sizeof(reg1);
+  outInst.o_operands[3] = sizeof(width) + sizeof(reg1) + sizeof(reg2);
+  outInst.o_operands[4] = 0;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 4;
+
+  reg1.size = sizeof(reg1);
+  reg1.kind = BrigEOperandReg;
+  reg1.type = Brigb1;
+  reg1.reserved = 0;
+  reg1.s_name = 0;
+
+  reg2.size = sizeof(reg2);
+  reg2.kind = BrigEOperandReg;
+  reg2.type = Brigb32;
+  reg2.reserved = 0;
+  reg2.s_name = reg1Name.size() + 1;
+
+  labRef.size = sizeof(labRef);
+  labRef.kind = BrigEOperandLabelRef;
+  labRef.labeldirective = 0;
+
+  Branch_Test<BrigInstBase, BrigOperandReg, BrigOperandReg, BrigOperandLabelRef> 
+  TestCase1(in, sbuf, &outInst, &width, &reg1, &reg2, &labRef);
+  TestCase1.Run_Test(&Branch);  
+  sbuf->clear();
+
+  /*************************** Case 2 ********************************/
+  in.assign("cbr_width(all)_fbar $c2, $s2, [%x];");
+  reg1Name.assign("$c2");   reg2Name.assign("$s2");
+  sbuf->append(reg1Name);   sbuf->append(reg2Name);
+
+  outMod.size = sizeof(outMod);
+  outMod.kind = BrigEInstMod;
+  outMod.opcode = BrigCbr;
+  outMod.type = Brigb32;
+  outMod.packing = BrigNoPacking;
+  outMod.o_operands[0] = 0;
+  outMod.o_operands[1] = sizeof(width);
+  outMod.o_operands[2] = sizeof(width) + sizeof(reg1);
+  outMod.o_operands[3] = sizeof(width) + sizeof(reg1) + sizeof(reg2);
+  outMod.o_operands[4] = 0;
+  memset(&outMod.aluModifier, 0, sizeof(outMod.aluModifier));
+  outMod.aluModifier.valid = 1;
+  outMod.aluModifier.fbar = 1;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 0;
+
+  reg1.size = sizeof(reg1);
+  reg1.kind = BrigEOperandReg;
+  reg1.type = Brigb1;
+  reg1.reserved = 0;
+  reg1.s_name = 0;
+
+  reg2.size = sizeof(reg2);
+  reg2.kind = BrigEOperandReg;
+  reg2.type = Brigb32;
+  reg2.reserved = 0;
+  reg2.s_name = reg1Name.size() + 1;
+
+  addr.size = sizeof(addr);
+  addr.kind = BrigEOperandAddress;
+  addr.type = Brigb64;
+  addr.reserved = 0;
+  addr.directive = 0;
+ 
+  Branch_Test<BrigInstMod, BrigOperandReg, BrigOperandReg, BrigOperandAddress> 
+  TestCase2(in, sbuf, &outMod, &width, &reg1, &reg2, &addr);
+  TestCase2.Run_Test(&Branch);  
+  sbuf->clear();
+
+  /*************************** Case 3 ********************************/
+
+  in.assign("cbr_width(2) $c0, @label;\n");
+  reg1Name.assign("$c0");
+  sbuf->append(reg1Name);
+
+  outInst.size = sizeof(outInst);
+  outInst.kind = BrigEInstBase;
+  outInst.opcode = BrigCbr;
+  outInst.type = Brigb32;
+  outInst.packing = BrigNoPacking;
+  outInst.o_operands[0] = 0;
+  outInst.o_operands[1] = sizeof(width);
+  outInst.o_operands[2] = sizeof(width) + sizeof(reg1);
+  outInst.o_operands[3] = 0;
+  outInst.o_operands[4] = 0;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 2;
+
+  reg1.size = sizeof(reg1);
+  reg1.kind = BrigEOperandReg;
+  reg1.type = Brigb1;
+  reg1.reserved = 0;
+  reg1.s_name = 0;
+
+  labRef.size = sizeof(labRef);
+  labRef.kind = BrigEOperandLabelRef;
+  labRef.labeldirective = 0;
+ 
+  Branch_Test<BrigInstBase, BrigOperandReg, BrigOperandLabelRef> 
+  TestCase3(in, sbuf, &outInst, &width, &reg1, &labRef);
+  TestCase3.Run_Test(&Branch);  
+  sbuf->clear();
+
+  /*************************** Case 4 ********************************/
+
+  in.assign("cbr $c1, $s1;\n");
+  reg1Name.assign("$c1");   reg2Name.assign("$s1");
+  sbuf->append(reg1Name);   sbuf->append(reg2Name);
+
+  outInst.size = sizeof(outInst);
+  outInst.kind = BrigEInstBase;
+  outInst.opcode = BrigCbr;
+  outInst.type = Brigb32;
+  outInst.packing = BrigNoPacking;
+  outInst.o_operands[0] = 0;
+  outInst.o_operands[1] = sizeof(width);
+  outInst.o_operands[2] = sizeof(width) + sizeof(reg1);
+  outInst.o_operands[3] = 0;
+  outInst.o_operands[4] = 0;
+
+  width.size = sizeof(width);
+  width.kind = BrigEOperandImmed;
+  width.type = Brigb32;
+  width.reserved = 0;
+  memset(&width.bits, 0, sizeof(width.bits));
+  width.bits.u = 0;
+
+  reg1.size = sizeof(reg1);
+  reg1.kind = BrigEOperandReg;
+  reg1.type = Brigb1;
+  reg1.reserved = 0;
+  reg1.s_name = 0;
+
+  reg2.size = sizeof(reg2);
+  reg2.kind = BrigEOperandReg;
+  reg2.type = Brigb32;
+  reg2.reserved = 0;
+  reg2.s_name = reg1Name.size() + 1;
+ 
+  Branch_Test<BrigInstBase, BrigOperandReg, BrigOperandReg> 
+  TestCase4(in, sbuf, &outInst, &width, &reg1, &reg2);
+  TestCase4.Run_Test(&Branch);  
+  sbuf->clear();
+
+
+  /***************************  End of tests ***********************/
   delete sbuf;
 }
  
