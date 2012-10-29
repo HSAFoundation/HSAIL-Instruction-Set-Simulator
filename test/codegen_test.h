aca4e84da3117cfdb40357da53c354c983daee27
Deepthi <none@none>
2012-10-30 00:32:23 +0530

    [Refactor] Unified Validate Function

--- test/codegen_test.h
+++ test/codegen_test.h
@@ -4,10 +4,11 @@
 #include "context.h"
 #include "brig_util.h"
 #include "brig_buffer.h"
+//#include "codegen_validate.h"
 
 namespace hsa{
 namespace brig{
-
+    
 class BrigCodeGenTest {
 protected:
   Context* context;
@@ -17,6 +18,25 @@ protected:
   struct BrigSections* RefOutput;
     
 public:
+
+#define caseInstBrig(X)                                   \
+  case BrigE ## X:                                    \
+    validate_brig::validate((cast<Brig ## X>(refcode)), (cast<Brig ## X>(getcode)),  \
+      RefOutput, TestOutput);   \
+    break
+/*
+#define caseDirBrig(X)                                   \
+  case BrigE ## X:                                    \
+    validate_brig::validate((cast<Brig ## X>(refdir)), (cast<Brig ## X>(getdir)),  \
+      RefOutput, TestOutput);   \
+    break
+
+#define caseOperBrig(X)                                   \
+  case BrigE ## X:                                    \
+    validate_brig::validate((cast<Brig ## X>(refoper)), (cast<Brig ## X>(getoper)),  \
+      RefOutput, TestOutput);   \
+    break    
+*/
   BrigCodeGenTest(std::string &In, StringBuffer* Sbuf = NULL, BrigSections* RefOut = NULL){  
     context = NULL;
     main_reporter = NULL;
@@ -25,7 +45,95 @@ public:
     RefOutput = RefOut;
   }  
   
-  virtual void validate(struct BrigSections* BrigOutput) = 0;
+  void validate(struct BrigSections* TestOutput){
+  
+    inst_iterator getcode = TestOutput->code_begin();
+    inst_iterator refcode = RefOutput->code_begin();
+    const inst_iterator getcode_end = TestOutput->code_end();
+    const inst_iterator refcode_end = RefOutput->code_end();
+    for(; (getcode != getcode_end) && (TestOutput->codeSize>8); getcode++, refcode++){
+      switch(getcode->kind){
+        caseInstBrig(InstBase);
+        caseInstBrig(InstMod) ;
+        caseInstBrig(InstCvt) ;
+        caseInstBrig(InstRead);
+        caseInstBrig(InstBar) ; 
+        caseInstBrig(InstLdSt);
+        caseInstBrig(InstCmp);
+        caseInstBrig(InstMem);
+        caseInstBrig(InstAtomic); 
+        caseInstBrig(InstAtomicImage);  
+        caseInstBrig(InstImage); 
+        default: 
+            printf("Invalid instruction\n");
+            exit(1);
+      }
+    }
+    
+    dir_iterator getdir = TestOutput->begin();
+    dir_iterator refdir = RefOutput->begin();
+    const dir_iterator getdir_end = TestOutput->end();
+    const dir_iterator refdir_end = RefOutput->end();  
+    for(; (getdir != getdir_end) && (TestOutput.directivesSize>8) ; getdir++, refdir++){
+      switch(getdir->kind){
+        caseDirBrig(DirectivePad);        
+        caseDirBrig(DirectiveFunction) ;        
+        caseDirBrig(DirectiveKernel);        
+        caseDirBrig(DirectiveSymbol);        
+        caseDirBrig(DirectiveImage) ;        
+        caseDirBrig(DirectiveSampler);        
+        caseDirBrig(DirectiveLabel) ;       
+        caseDirBrig(DirectiveLabelList) ;        
+        caseDirBrig(DirectiveVersion);         
+        caseDirBrig(DirectiveSignature);          
+        caseDirBrig(DirectiveFile) ;        
+        caseDirBrig(DirectiveComment);        
+        caseDirBrig(DirectiveLoc);        
+        caseDirBrig(DirectiveInit);        
+        caseDirBrig(DirectiveLabelInit);        
+        caseDirBrig(DirectiveControl);        
+        caseDirBrig(DirectivePragma);       
+        caseDirBrig(DirectiveExtension);        
+        caseDirBrig(DirectiveArgStart);        
+        caseDirBrig(DirectiveArgEnd);
+        caseDirBrig(DirectiveBlockStart);        
+        caseDirBrig(DirectiveBlockNumeric);       
+        caseDirBrig(DirectiveBlockString);        
+        caseDirBrig(DirectiveBlockEnd);
+        default: 
+            printf("Invalid instruction\n");
+            exit(1);        
+      }      
+    }  
+    
+    oper_iterator getoper = TestOutput->oper_begin();
+    oper_iterator refoper = RefOutput->oper_begin();
+    const oper_iterator getoper_end = TestOutput->oper_end();
+    const oper_iterator refoper_end = RefOutput->oper_end();
+    for(; (getoper != getoper_end) && (TestOutput.operandsSize>8); getoper++, refoper++){
+      switch(refoper->kind){
+        caseOperBrig(OperandPad);
+        caseOperBrig(OperandBase);
+        caseOperBrig(OperandReg);
+        caseOperBrig(OperandImmed);
+        caseOperBrig(OperandRegV2);
+        caseOperBrig(OperandRegV4);
+        caseOperBrig(OperandAddress);
+        caseOperBrig(OperandLabelRef);
+        caseOperBrig(OperandIndirect);
+        caseOperBrig(OperandCompound);
+        caseOperBrig(OperandArgumentList);
+        caseOperBrig(OperandFunctionList);
+        caseOperBrig(OperandArgumentRef);
+        caseOperBrig(OperandWaveSz);
+        caseOperBrig(OperandFunctionRef);
+        caseOperBrig(OperandOpaque);
+        default: 
+          printf("Invalid Operand\n");
+          exit(1);
+      }
+    }
+  }
   
   void Run_Test(int (*Rule)(Context*)){
     main_reporter = ErrorReporter::get_instance();
