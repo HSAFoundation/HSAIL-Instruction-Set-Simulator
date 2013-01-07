b8e3a9edb56ce7849cf03da2084e76b08e4fb014
Thomas B. Jablin <tom@multicorewareinc.com>
2013-01-07 15:44:37 -0600

    [Refactor] Add brig_validate

---
+++ test/brig_validate.cc
@@ -0,0 +1,21 @@
+#include "brig_reader.h"
+#include "brig_module.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+  if(argc < 2) {
+    std::cerr << argv[0] << ": Missing input!\n";
+    return 1;
+  }
+
+  hsa::brig::BrigReader *reader =
+    hsa::brig::BrigReader::createBrigReader(argv[1]);
+  hsa::brig::BrigModule mod(*reader, &llvm::errs());
+  if(!mod.isValid())
+    std::cerr << argv[0] << ": Input is invalid!\n";
+
+  return 0;
+}
