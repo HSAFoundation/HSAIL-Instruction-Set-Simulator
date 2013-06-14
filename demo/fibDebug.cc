e0cfb913665f0ace8c47ee1aa4bd498d5a49edb4
Thomas B. Jablin <tom@multicorewareinc.com>
2013-06-14 09:27:46 -0500

    [Cleanup] Add licensing text.

--- demo/fibDebug.cc
+++ demo/fibDebug.cc
@@ -1,3 +1,12 @@
+//===- brigDebug.cc -------------------------------------------------------===//
+//
+//                     The HSA Simultor
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "brig_engine.h"
 #include "brig_llvm.h"
 #include "brig_module.h"
