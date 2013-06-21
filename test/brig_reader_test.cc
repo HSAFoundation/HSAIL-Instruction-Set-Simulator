ba4e2d65c39a2114bceaed988604b0b2ea351fd0
Huy <huy@multicorewareinc.com>
2013-06-21 16:16:30 -0500

    [HSA-648 Fix] Add brig_reader_test for Lastbit_u32_u64

--- test/brig_reader_test.cc
+++ test/brig_reader_test.cc
@@ -5917,6 +5917,34 @@ TEST(Instruction2Test, LastBit) {
     const int32_t testVec[] = { -1, 0x0};
     testInst("lastbit_u32_u32", testVec);
   }
+  
+  {
+    hsa::brig::BrigProgram BP = TestHSAIL(
+    "version 0:96:$full:$small;\n"
+    "\n"
+    "kernel &__test_kernel(\n"
+    "        kernarg_u32 %result, \n"
+    "        kernarg_u32 %input1)\n"
+    "{\n"
+    "        lastbit_u32_u64 $s1, 0x7fe0000000000;\n"
+    "        ld_kernarg_u32 $s0, [%result] ;\n"
+    "        st_u32 $s1, [$s0] ;\n"
+
+    "        ret;\n"
+    "};\n");
+    EXPECT_TRUE(BP);
+    if (!BP) return;
+
+    hsa::brig::BrigEngine BE(BP);
+    llvm::Function *fun = BP->getFunction("__test_kernel");
+    uint32_t *arg0 = new uint32_t(0);
+    uint32_t *arg1 = new uint32_t(0);
+    void *args[] = { &arg0, arg1 };
+    BE.launch(fun, args);
+    EXPECT_EQ(41, *arg0);
+    delete arg0;
+    delete arg1;
+  }
 }
 
 TEST(Instruction2Test, Mov) {
