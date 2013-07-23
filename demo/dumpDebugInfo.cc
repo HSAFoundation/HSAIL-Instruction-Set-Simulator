5945e2ae7033d4a79475ce80ca3a04dd7db629ca
Thomas B. Jablin <tom@multicorewareinc.com>
2013-07-23 16:45:15 -0500

    [Cleanup] Correct comment and other minor style issues.

--- demo/dumpDebugInfo.cc
+++ demo/dumpDebugInfo.cc
@@ -1,4 +1,4 @@
-//===- brigDebug.cc -------------------------------------------------------===//
+//===- dumpDebugInfo.cc ---------------------------------------------------===//
 //
 //                     The HSA Simulator
 //
@@ -14,7 +14,6 @@
 #include "brig_runtime.h"
 #include "brig_inst_helper.h"
 
-
 #include "llvm/IR/Module.h"
 #include "llvm/DIBuilder.h"
 #include "llvm/DebugInfo.h"
@@ -22,46 +21,42 @@
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/FileSystem.h"
 
-#define STR(X) #X
-#define XSTR(X) STR(X)
-
-#include <iostream>
-
 using namespace std;
 int main(int argc, char **argv) {
   if (argc < 2) {
-    std::cerr << "Missing input." << std::endl;
+    llvm::errs() << "Missing input.\n";
     return 0;
   }
 
   hsa::brig::BrigReader *reader =
     hsa::brig::BrigReader::createBrigReader(argv[1]);
   if (!reader) {
-    std::cerr << argv[0] << ": File not found: " << argv[1] << "\n";
+    llvm::errs() << argv[0] << ": File not found: " << argv[1] << "\n";
     return 0;
   }
 
   hsa::brig::BrigModule mod(*reader);
   if (!mod.isValid()) {
-    std::cerr << argv[0] << ": Input is invalid!\n";
+    llvm::errs() << argv[0] << ": Input is invalid!\n";
     return 0;
   }
 
   hsa::brig::BrigInstHelper helper = mod.getInstHelper();
   for (hsa::brig::debug_iterator it = mod.debug_begin(),
        E = mod.debug_end(); it != E; ++it) {
-    if (const BrigBlockNumeric *numeric = hsa::brig::dyn_cast<BrigBlockNumeric>(it)) {
+    if (const BrigBlockNumeric *numeric =
+        hsa::brig::dyn_cast<BrigBlockNumeric>(it)) {
       const BrigString *str = helper.getData(numeric);
       llvm::StringRef debugData((const char *) str->bytes, str->byteCount);
       llvm::MemoryBuffer *debugDataBuffer =
         llvm::MemoryBuffer::getMemBuffer(debugData, "", false);
       std::string err;
       std::string input = std::string(argv[1]);
-      std::string filename = input.substr(0, input.length()-2);     
+      std::string filename = input.substr(0, input.length()-2);
       filename.append(".debuginfo");
 
       llvm::raw_fd_ostream fstream(filename.c_str(), err, 2);
-      fstream << debugDataBuffer->getBuffer() ;      
+      fstream << debugDataBuffer->getBuffer() ;
       return 0;
     }
   }
