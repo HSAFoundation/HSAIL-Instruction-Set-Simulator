d89f01ac5510ca7b2faf10e6ebabbbffa9b69c15
Thomas B. Jablin <tom@multicorewareinc.com>
2013-07-29 15:06:17 -0500

    [HSA-681 Fix] Test for existence of libelf.h in Cmake.
    Display a friendly error message if libelf.h is not found.

--- src/brig2llvm/scripts/cmake/EnsureELFisPresent.cmake
+++ src/brig2llvm/scripts/cmake/EnsureELFisPresent.cmake
@@ -10,3 +10,14 @@ int main(int argc, char **argv) {
 if ( NOT HAS_ELF )
   MESSAGE(FATAL_ERROR "elf.h is missing")
 endif()
+
+CHECK_CXX_SOURCE_COMPILES("
+\#include <libelf.h>
+int main(int argc, char **argv) {
+        return 0;
+      }
+" HAS_LIBELF)
+
+if ( NOT HAS_LIBELF )
+  MESSAGE(FATAL_ERROR "libelf.h is missing")
+endif()
